cc917a82ed4a8774183175e9be4e6e1350f6e0fd
Tushar Mankar <tusharmankar02@gmail.com>
2025-04-09 17:42:05 +0530

    add create_project command

--- pyqtforge/cli.py
+++ pyqtforge/cli.py
@@ -1,10 +1,19 @@
 import typer
-
+from generator import generate_project
 app = typer.Typer()
+
+
 
 @app.command()
 def create(name: str):
+    """start project"""
     typer.echo(f"🔧 Creating new PyQt app: {name}")
+
+@app.command("createproject")
+def create_project(name: str):
+    """Create a new PyQt project with the given name."""
+    generate_project(name)
+
 
 
 if __name__ == "__main__":
