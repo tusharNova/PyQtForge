48f04c5d25f13a073b82741f09e42ae60a44dbf1
Tushar Mankar <tusharmankar02@gmail.com>
2025-04-24 19:41:46 +0530

    change in version  of pyqtfroge

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup , find_packages
 
 setup(
     name = "pyqtforge",
-    version="0.1.0",
+    version="0.1.2",
     package = find_packages(),
     include_package_data=True,
     install_requires=[
