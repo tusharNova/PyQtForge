ef138558bcfaa6941fe260e5fc9c2a670eb6d4d3
Tushar Mankar <tusharmankar02@gmail.com>
2025-04-27 20:40:55 +0530

    change in packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup , find_packages
 
 setup(
     name = "pyqtforge",
-    version="0.1.3",
+    version="0.1.4",
     package = find_packages(),
     include_package_data=True,
     install_requires=[
@@ -20,11 +20,12 @@ setup(
 },
     author = "Tushar Mankar",
     description="A CLI tool to scaffold and manage PyQt projects easily.",
-
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
+    url = "https://github.com/tusharNova/PyQtForge",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
