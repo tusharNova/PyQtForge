ed72bef0d27cd06ac9d0726d767b5ea86f304984
Tushar Mankar <tusharmankar02@gmail.com>
2025-05-01 21:35:22 +0530

    testing the controller code

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from controller.ClsLogin import ClsLogin
+
+if __name__ == '__main__':
+    app = QApplication([])
+    f = ClsLogin()
+    f.show()
+    sys.exit(app.exec_())
