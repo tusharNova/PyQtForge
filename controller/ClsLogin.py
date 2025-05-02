fd2cd37f5b0aaff624b23c6c0b7ce10b99811b54
Tushar Mankar <tusharmankar02@gmail.com>
2025-05-02 20:41:29 +0530

    change in version of pyqtforge and upload dist

--- controller/ClsLogin.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QMainWindow
-from views.login import Ui_MainWindow
-
-class ClsLogin(QMainWindow):
-    def __init__(self):
-        super(ClsLogin ,self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup , find_packages
 
 setup(
     name = "pyqtforge",
-    version="0.1.5",
+    version="0.1.6",
     package = find_packages(),
     include_package_data=True,
     install_requires=[
