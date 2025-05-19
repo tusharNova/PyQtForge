8e3dbab5f82d6d8651d5fb2d891669b1ee7f6e96
Tushar Mankar <tusharmankar02@gmail.com>
2025-05-19 19:26:11 +0530

    update files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup , find_packages
 
 setup(
     name = "pyqtforge",
-    version="0.1.8",
+    version="0.1.9",
     package = find_packages(),
     include_package_data=True,
     install_requires=[
